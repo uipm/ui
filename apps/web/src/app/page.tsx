38782f45d6bacf7e9714a06aa40efa9e67683ed3
hduc-codeserver <hducit@gmail.com>
2025-06-19 14:08:01 +0700

    update ui-web

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
-import { Autocomplete, Button } from 'ui-web'
-import TableAutocomplete, { Column } from "ui-web/Autocomplete/TableAutocomplete";
-const columns: Column[] = [
-  { id: 'id', label: 'Mã' },
-  { id: 'name', label: 'Tên' },
-]
+import { Autocomplete, Button } from '@uipm/ui-web'
+// import TableAutocomplete, { Column } from "@uipm/ui-web/Autocomplete/TableAutocomplete";
+// const columns: Column[] = [
+//   { id: 'id', label: 'Mã' },
+//   { id: 'name', label: 'Tên' },
+// ]
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button label="abc" />
+      <Button variant="primary">Click Me</Button>
         <Autocomplete />
-        <TableAutocomplete columns={columns} data={[{ id: 12, name: 'mã tôi là' }]} onChange={() => { }} />
+        {/* <TableAutocomplete columns={columns} data={[{ id: 12, name: 'mã tôi là' }]} onChange={() => { }} /> */}
       </main>
     </div>
   );
